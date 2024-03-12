2cc9b7d55830f98697e4c1587c60c7fcaa2e1741
guswls78963210 <guswls78963210@gmail.com>
2024-03-12 23:33:01 +0900

    [TEST-#1] CI 테스트용 Test 클래스 추가

---
+++ src/test/java/tddproject/parking/CiTest.java
@@ -0,0 +1,18 @@
+package tddproject.parking;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CiTest {
+
+	@Test
+	void success(){
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	void fail(){
+		assertThat(1).isEqualTo(0);
+	}
+}
